aa2bb3713c93e8765bf0f1b307209f23ff4f3951
iknahar <knahar.kamrun@gmail.com>
2022-12-31 02:13:56 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+user_propmt = "Enter a to do:"
+
+todo1 = input(user_propmt)
+todo2 = input(user_propmt)
+todo3 = input(user_propmt)
+
+todos = [todo1, todo2, todo3, "hellow"]
+print(todos)
+
+print(type(todo1))
